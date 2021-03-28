340a85900cd935ccf463382d23be488438ba3ccd
Dario Passariello <dariopassariello@gmail.com>
2021-03-28 10:39:04 -0700

    Update compressor.php

--- compressor.php
+++ compressor.php
@@ -25,31 +25,31 @@ INTASTALLATION:
 PUT AS INCLUDE OR REQUIRE AT TOP OF INDEX FILE
 */
 
-	function compression( $html ){
+function compression( $html ){
 
-		$html = preg_replace( '/<!-(.*)->/Uis', '', $html );
-		$html = preg_replace( '/[\r\n\t\s]+/s', ' ', $html );
-		$html = preg_replace( '#/\*.*?\*/#', ' ', $html );
-		$html = preg_replace( '/^\s+/', ' ', $html );
-		$html = preg_replace( '/ +/', ' ', $html );
-		$html = preg_replace( '/> +</', '><', $html );
-		$html = preg_replace( '/[[:blank:]]+/', ' ', $html );
-		$html = preg_replace( '/<!--[if IE]>/', $html, $html );
-		$html = preg_replace( '/<![endif]-->/', $html, $html );
+$html = preg_replace( '/<!-(.*)->/Uis', '', $html );
+$html = preg_replace( '/[\r\n\t\s]+/s', ' ', $html );
+$html = preg_replace( '#/\*.*?\*/#', ' ', $html );
+$html = preg_replace( '/^\s+/', ' ', $html );
+$html = preg_replace( '/ +/', ' ', $html );
+$html = preg_replace( '/> +</', '><', $html );
+$html = preg_replace( '/[[:blank:]]+/', ' ', $html );
+$html = preg_replace( '/<!--[if IE]>/', $html, $html );
+$html = preg_replace( '/<![endif]-->/', $html, $html );
 
-		$html = str_replace( '{!','<!', $html );
-		$html = str_replace( '--}','-->', $html );
-		$html = str_replace( ']}',']>', $html) ;
-		$html = preg_replace_callback( '/(&#[0-9]+;)/', function( $m ){ 
-      return mb_convert_encoding( $m[1], $GLOBALS['CHAR_ENCODE'], 'HTML-ENTITIES' );
-    }, $html);
+$html = str_replace( '{!','<!', $html );
+$html = str_replace( '--}','-->', $html );
+$html = str_replace( ']}',']>', $html) ;
+$html = preg_replace_callback( '/(&#[0-9]+;)/', function( $m ){ 
+return mb_convert_encoding( $m[1], $GLOBALS['CHAR_ENCODE'], 'HTML-ENTITIES' );
+}, $html);
 
-		clearstatcache();
-    return trim( $html );
+clearstatcache();
+return trim( $html );
 
-	}
+}
 
-	ob_start( 'compression' );
-	ob_implicit_flush(1);
+ob_start( 'compression' );
+ob_implicit_flush(1);
 
 ?>
