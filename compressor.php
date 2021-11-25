76a03083054ded0a68f9067f91d0f1bfc786e2ff
Dario Passariello <dariopassariello@gmail.com>
2021-11-25 12:32:59 -0800

    Update compressor.php

--- compressor.php
+++ compressor.php
@@ -1,10 +1,7 @@
-<?php
-
+<?
 /*
-Created by Dario Passariello
-
 The MIT License (MIT)
-Copyright (c) 2016 - 2021 Dario Passariello
+Copyright (c) 2016 Dario Passariello
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -20,36 +17,44 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
-INTASTALLATION: 
-PUT AS INCLUDE OR REQUIRE AT TOP OF INDEX FILE
 */
 
-function compression( $html ){
-
-$html = preg_replace( '/<!-(.*)->/Uis', '', $html );
-$html = preg_replace( '/[\r\n\t\s]+/s', ' ', $html );
-$html = preg_replace( '#/\*.*?\*/#', ' ', $html );
-$html = preg_replace( '/^\s+/', ' ', $html );
-$html = preg_replace( '/ +/', ' ', $html );
-$html = preg_replace( '/> +</', '><', $html );
-$html = preg_replace( '/[[:blank:]]+/', ' ', $html );
-$html = preg_replace( '/<!--[if IE]>/', $html, $html );
-$html = preg_replace( '/<![endif]-->/', $html, $html );
-
-$html = str_replace( '{!','<!', $html );
-$html = str_replace( '--}','-->', $html );
-$html = str_replace( ']}',']>', $html) ;
-$html = preg_replace_callback( '/(&#[0-9]+;)/', function( $m ){ 
-return mb_convert_encoding( $m[1], $GLOBALS['CHAR_ENCODE'], 'HTML-ENTITIES' );
-}, $html);
-
-clearstatcache();
-return trim( $html );
-
-}
-
-ob_start( 'compression' );
+ob_start("compression");
 ob_implicit_flush(1);
 
+function compression($html){
+
+mb_substitute_character("none");
+mb_internal_encoding($GLOBALS['CHAR_ENCODE']);
+mb_http_output($GLOBALS['CHAR_ENCODE']);
+mb_http_input($GLOBALS['CHAR_ENCODE']);
+mb_regex_encoding($GLOBALS['CHAR_ENCODE']);
+
+$html = mb_convert_encoding($html, $GLOBALS['CHAR_ENCODE'], $GLOBALS['CHAR_ENCODE']);
+$html = htmlspecialchars_decode(htmlspecialchars($html, ENT_SUBSTITUTE, $GLOBALS['CHAR_ENCODE']));
+
+$html = SpecialCharacter($html);
+$html = utf8_encode($html);
+
+$html = preg_replace('/<!-(.*)->/Uis', '', $html);
+$html = preg_replace('/[\r\n\t\s]+/s', ' ', $html);
+$html = preg_replace('#/\*.*?\*/#', ' ', $html);
+$html = preg_replace('/^\s+/', ' ', $html);
+$html = preg_replace('/ +/', ' ', $html);
+$html = preg_replace('/> +</', '><', $html);
+$html = preg_replace('/[[:blank:]]+/', ' ', $html);
+$html = preg_replace('/<!--[if IE]>/', $html, $html);
+$html = preg_replace('/<![endif]-->/', $html, $html);
+
+$html = str_replace('{!','<!', $html);
+$html = str_replace('--}','-->', $html);
+$html = str_replace(']}',']>', $html);
+$html = preg_replace_callback("/(&#[0-9]+;)/", function($m){return mb_convert_encoding($m[1], $GLOBALS['CHAR_ENCODE'], "HTML-ENTITIES");}, $html);
+$html = preg_replace_callback("/([a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})/", function($m){return mb_convert_encoding($m[1], $GLOBALS['CHAR_ENCODE'], "HTML-ENTITIES");}, $html);
+
+clearstatcache();
+flush();
+
+return trim($html);
+}
 ?>
